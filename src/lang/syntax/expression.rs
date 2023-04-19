ebfacec220f41f5e00ab7388049103e7232da930
Paul <vautier.paul14350@gmail.com>
2023-04-19 21:31:12 +0200

    Expression parsing : left recursive parsing with top down descent (/!\ overflows on incorrect input)

--- src/lang/syntax/expression.rs
+++ src/lang/syntax/expression.rs
@@ -1,28 +1,50 @@
 use std::process::Output;
 
 use pepser::{
+    errors::ParserError,
     impls::{none_of, sequence, ws},
-    traits::{discard, opt, value, wrapped, ParseResult, Parser},
+    traits::{discard, opt, sep_by, value, wrapped, Input, ParseResult, Parser},
 };
 
+macro_rules! bin_op{
+    ($function:ident, ($first_seq:expr, $first_binop:expr) $(,($seq:expr, $binop:expr))*) => {
+        {
+            let or_combinator = value($first_binop, sequence($first_seq));
+
+            $(
+                let or_combinator = or_combinator.or(value($binop, sequence($seq)));
+            )*
+
+            $function.and(
+                opt(
+                    wrapped(
+                        ws(),
+                        or_combinator,
+                        ws(),
+                        ).and($function)))
+                .map(|(expr, opt)| {
+                    opt.map_or(expr.clone(), |(binop, other_expr)| {
+                        Expr::Binary(binop, Box::new(expr), Box::new(other_expr))
+                    })
+                })
+        }
+    };
+}
 use super::lexical_grammar::{identifier, num_literal, string_literal};
 
 #[derive(Debug, Clone)]
 pub enum Expr {
     Str(String),
     Value(f64),
+    Bool(bool),
     Assignement(String, Box<Expr>),
     Binary(BinaryOperator, Box<Expr>, Box<Expr>),
-    Or,
-    And,
-    Eq,
-    Comp,
-    Term,
-    Factor,
-    Unary,
-    Call,
-    Primary,
+    Ident(String),
+    Unary(UnaryOperator, Box<Expr>),
+    Call(Box<Expr>, Vec<Expr>),
 }
+
+#[derive(Clone, Debug)]
 pub enum UnaryOperator {
     Neg,
     Bang,
@@ -35,15 +57,16 @@ pub enum BinaryOperator {
     Div,
     Sub,
     Or,
-    And,
+    Eq,
+    Neq,
+    Leq,
+    Lt,
+    Geq,
+    Gt,
 }
 
-pub fn next<'a>(input: &'a str) -> ParseResult<&'a str, Expr> {
-    ws().map(|_| Expr::Call).parse(input)
-}
-
-pub fn parse_language<'a>(input: &'a str) -> ParseResult<&'a str, Expr> {
-    wrapped(ws(), expression, ws()).parse(input)
+pub fn parse_language<'a>(input: &'a str) -> ParseResult<&'a str, Vec<Expr>> {
+    wrapped(ws(), sep_by(non_empty(expression), sequence(";")), ws()).parse(input)
 }
 
 pub fn expression<'a>(input: &'a str) -> ParseResult<&'a str, Expr> {
@@ -58,122 +81,106 @@ pub fn assignement<'a>(input: &'a str) -> ParseResult<&'a str, Expr> {
         .parse(input)
 }
 
-#[rustfmt::skip]
-pub fn logic_or<'a>(
-    input: &'a str
-) -> ParseResult<&'a str, Expr>
-{
-    logic_and.and(
-        opt(
-            wrapped(
-                ws(),
-                value(BinaryOperator::Or, sequence("||")),
-                ws(),
-                ).and(logic_and)))
-        .map(|(expr, opt)| {
-            opt.map_or(expr.clone(), |(binop, other_expr)| {
-                Expr::Binary(binop, Box::new(expr), Box::new(other_expr))
-            })
-        })
-        .parse(input)
+pub fn logic_or<'a>(input: &'a str) -> ParseResult<&'a str, Expr> {
+    bin_op!(logic_and, ("||", BinaryOperator::Or)).parse(input)
 }
 
-#[rustfmt::skip]
-pub fn logic_and<'a>(
-    input: &'a str
-) -> ParseResult<&'a str, Expr>
-{
-    equality.and(
-        opt(
-            wrapped(
-                ws(),
-                value(BinaryOperator::Or, sequence("||")),
-                ws(),
-                ).and(equality)))
-        .map(|(expr, opt)| {
-            opt.map_or(expr.clone(), |(binop, other_expr)| {
-                Expr::Binary(binop, Box::new(expr), Box::new(other_expr))
-            })
-        })
-        .parse(input)
+pub fn logic_and<'a>(input: &'a str) -> ParseResult<&'a str, Expr> {
+    bin_op!(equality, ("&&", BinaryOperator::Or)).parse(input)
 }
 
-#[rustfmt::skip]
-pub fn equality<'a>(
-    input: &'a str
-) -> ParseResult<&'a str, Expr>
-{
-    comparison.and(
-        opt(
-            wrapped(
-                ws(),
-                value(BinaryOperator::Or, sequence("||")),
-                ws(),
-                ).and(comparison)))
-        .map(|(expr, opt)| {
-            opt.map_or(expr.clone(), |(binop, other_expr)| {
-                Expr::Binary(binop, Box::new(expr), Box::new(other_expr))
-            })
-        })
-        .parse(input)
+pub fn equality<'a>(input: &'a str) -> ParseResult<&'a str, Expr> {
+    bin_op!(
+        comparison,
+        ("==", BinaryOperator::Eq),
+        ("!==", BinaryOperator::Neq)
+    )
+    .parse(input)
 }
 
-#[rustfmt::skip]
-pub fn comparison<'a>(
-    input: &'a str
-) -> ParseResult<&'a str, Expr>
-{
-    term.and(
-        opt(
-            wrapped(
-                ws(),
-                value(BinaryOperator::Or, sequence("||")),
-                ws(),
-                ).and(term)))
-        .map(|(expr, opt)| {
-            opt.map_or(expr.clone(), |(binop, other_expr)| {
-                Expr::Binary(binop, Box::new(expr), Box::new(other_expr))
-            })
-        })
-        .parse(input)
+pub fn comparison<'a>(input: &'a str) -> ParseResult<&'a str, Expr> {
+    bin_op!(
+        term,
+        ("<", BinaryOperator::Lt),
+        ("<=", BinaryOperator::Leq),
+        (">", BinaryOperator::Gt),
+        (">=", BinaryOperator::Geq)
+    )
+    .parse(input)
 }
 
-#[rustfmt::skip]
-pub fn term<'a>(
-    input: &'a str
-) -> ParseResult<&'a str, Expr>
-{
-    equality.and(
-        opt(
-            wrapped(
-                ws(),
-                value(BinaryOperator::Or, sequence("||")),
-                ws(),
-                ).and(equality)))
-        .map(|(expr, opt)| {
-            opt.map_or(expr.clone(), |(binop, other_expr)| {
-                Expr::Binary(binop, Box::new(expr), Box::new(other_expr))
-            })
-        })
-        .parse(input)
+pub fn term<'a>(input: &'a str) -> ParseResult<&'a str, Expr> {
+    bin_op!(
+        factor,
+        ("-", BinaryOperator::Sub),
+        ("+", BinaryOperator::Add)
+    )
+    .parse(input)
 }
 
-#[rustfmt::skip]
-pub fn factor<'a>(
-    input: &'a str
-) -> ParseResult<&'a str, Expr>
+pub fn factor<'a>(input: &'a str) -> ParseResult<&'a str, Expr> {
+    bin_op!(
+        unary,
+        ("/", BinaryOperator::Div),
+        ("*", BinaryOperator::Mul)
+    )
+    .parse(input)
+}
+
+pub fn unary<'a>(input: &'a str) -> ParseResult<&'a str, Expr> {
+    wrapped(
+        ws(),
+        value(UnaryOperator::Bang, sequence("!"))
+            .or(value(UnaryOperator::Neg, sequence("-")))
+            .and(unary)
+            .map(|(operator, expr)| Expr::Unary(operator, Box::new(expr)))
+            .or(call),
+        ws(),
+    )
+    .parse(input)
+}
+
+pub fn call<'a>(input: &'a str) -> ParseResult<&'a str, Expr> {
+    wrapped(
+        ws(),
+        primary
+            .and(opt(discard(
+                wrapped(ws(), sequence(":>"), ws()),
+                sep_by(expression, wrapped(ws(), sequence("::"), ws())),
+            )))
+            .map(|(primary, args)| Expr::Call(Box::new(primary), args.unwrap_or(vec![]))),
+        ws(),
+    )
+    .parse(input)
+}
+
+pub fn primary<'a>(input: &'a str) -> ParseResult<&'a str, Expr> {
+    wrapped(
+        ws(),
+        value(Expr::Bool(true), sequence("true"))
+            .or(value(Expr::Bool(false), sequence("false")))
+            .or(num_literal)
+            .or(string_literal)
+            .or(identifier.map(Expr::Ident))
+            .or(expression),
+        ws(),
+    )
+    .parse(input)
+}
+
+pub fn non_empty<P, I: Input>(mut parser: P) -> impl FnMut(I) -> ParseResult<I, P::Output>
+where
+    P: Parser<I>,
 {
-    equality.and(
-        opt(
-            wrapped(
-                ws(),
-                value(BinaryOperator::Or, sequence("||")),
-                ws(),
-                ).and(equality)))
-        .map(|(expr, opt)| {
-            opt.map_or(expr.clone(), |(binop, other_expr)| {
-                Expr::Binary(binop, Box::new(expr), Box::new(other_expr))
-            })
-        })
-        .parse(input)
+    move |input: I| {
+        if input.input_len() == 0 {
+            return Err(ParserError::new(
+                0,
+                pepser::errors::ErrorSource::Many,
+                "empty",
+            ));
+        }
+
+        parser.parse(input)
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,5 @@ use crate::lang::syntax::parse_language;
 mod lang;
 
 fn main() {
-    println!("{:?}", parse_language("\"_asdasd332_3232'\""));
+    println!("{:?}", parse_language("3 :> 3 :: 4"));
 }
